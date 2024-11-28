818e190d6c0456e41e356204ae3fa7b92cef94ed
alifzee <136773602+alifzee@users.noreply.github.com>
2024-11-28 06:29:44 -0800

    Created using Colab

---
+++ app.py
@@ -0,0 +1,44 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPJQc1c2i/M2PQyFegMEQlY"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "source": [
+        "!streamlit run app.py & npx localtunnel --port 8501 & curl ipv4.icanhazip.com\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "w2CoZQNg_Jvj",
+        "outputId": "19fc4cd8-d9fd-4875-9bd7-5b5f00494785"
+      },
+      "execution_count": 5,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "/bin/bash: line 1: streamlit: command not found\n",
+            "34.19.75.43\n",
+            "\u001b[1G\u001b[0K⠙\u001b[1G\u001b[0K⠹\u001b[1G\u001b[0K⠸\u001b[1G\u001b[0K⠼\u001b[1G\u001b[0K⠴\u001b[1G\u001b[0Kyour url is: https://puny-icons-fold.loca.lt\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
